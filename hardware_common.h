259a99aa7b5584204152905e47229bd0ec0b1138
LEVE1S <125179774+LEVE1S@users.noreply.github.com>
2025-06-10 18:00:13 +0800

    Add files via upload

---
+++ hardware_common.h
@@ -0,0 +1,9 @@
+#ifndef __HARDWARE_COMMEN_H_
+#define __HARDWARE_COMMEN_H_
+
+#include "stm32f10x.h"                  // Device header
+
+extern volatile uint8_t Buzzer_Flag = 0;
+extern volatile uint16_t Counter = 0;
+
+#endif
